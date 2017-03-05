96e3339bfb425e6769ad689cc44c3caf86370521
Stan Chapski <stanchapski@mindspring.com>
2017-03-04 19:04:20 -0800

    added hello world and test

---
+++ R/hello_world.R
@@ -0,0 +1,6 @@
+#' Hellow world
+#'
+#' @export
+hello_world <- function() {
+  cat("Hello World\n")
+}
---
+++ tests/testthat/test_hello_world.R
@@ -0,0 +1,5 @@
+# test_hello_world
+
+test_that("hello world works", {
+  expect_output(hello_world(), "Hello World")
+})
